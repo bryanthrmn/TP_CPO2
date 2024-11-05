ca2122faa6289a751976f0cd4038deeffe5611ff
Bryan THIRIMANNA <85101130+bryanthrmn@users.noreply.github.com>
2024-11-05 15:34:32 +0100

    Update PrestationSale.java

--- TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/PrestationSale.java
+++ TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/PrestationSale.java
@@ -1,5 +1,24 @@
 package tp3;
 
-public class PrestationSale {
-    
+public class PrestationSale extends Prestation {
+    public PrestationSale(char categorieVehicule) {
+        super(categorieVehicule);
+    }
+
+    @Override
+    public double calculerPrixTotal() {
+        return calculerPrelavage() + calculerLavage() + calculerSechage();
+    }
+
+    private double calculerPrelavage() {
+        // Calcul du prix du prélavage selon la catégorie
+    }
+
+    private double calculerLavage() {
+        // Calcul du prix du lavage selon la catégorie
+    }
+
+    private double calculerSechage() {
+        // Calcul du prix du séchage selon la catégorie
+    }
 }
