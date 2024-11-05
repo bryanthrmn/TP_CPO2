18991eba831e21e9717fbf2ac6a8d941d08ec19f
Bryan THIRIMANNA <85101130+bryanthrmn@users.noreply.github.com>
2024-11-05 12:53:41 +0100

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,24 @@
+
+public class Client {
+   //Attributs
+    private int numero;
+    private String nom, tel, email;
+    
+    //Constructeurs
+    public Client(int numero, String nom, String tel){
+        this.numero = numero;
+        this.tel = tel;
+        this.email = null;
+    }
+    
+    public Client(int numero, String nom, String tel, String email){
+        
+    }
+    
+    public String toString(){
+    return "numero =" + numero
+           + "nom =" + nom
+           + "tel =" + tel
+           + "email =" + email;
+    }
+}
