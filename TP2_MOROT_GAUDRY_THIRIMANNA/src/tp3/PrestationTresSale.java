8588d747a28c8f5b498c8e433361565a2f6f314e
Bryan THIRIMANNA <85101130+bryanthrmn@users.noreply.github.com>
2024-11-05 15:35:01 +0100

    Update PrestationTresSale.java

--- TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/PrestationTresSale.java
+++ TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/PrestationTresSale.java
@@ -1,5 +1,21 @@
 package tp3;
 
-public class PrestationTresSale {
-    
+public class PrestationTresSale extends PrestationSale {
+    private int typeSalissure; // 1 à 4
+
+    public PrestationTresSale(char categorieVehicule, int typeSalissure) {
+        super(categorieVehicule);
+        this.typeSalissure = typeSalissure;
+    }
+
+    @Override
+    public double calculerPrixTotal() {
+        double prix = super.calculerPrixTotal();
+        prix += calculerSurcoutTypeSalissure();
+        return prix;
+    }
+
+    private double calculerSurcoutTypeSalissure() {
+        // Calcul du surcoût selon le type de salissure
+    }
 }
