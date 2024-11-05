ee898dfac413f54e16e474457327b11e87d6121c
Bryan THIRIMANNA <85101130+bryanthrmn@users.noreply.github.com>
2024-11-05 15:30:34 +0100

    Update Client.java

--- TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/Client.java
+++ TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/Client.java
@@ -1,30 +1,43 @@
 package tp3;
 
-public class Client {
-   //Attributs
-    private int numero;
-    private String nom, tel, email;
-    
-    //Constructeurs
-    public Client(int numero, String nom, String tel){
-        this.numero = numero;
+
+  public class Client {
+    private static int clientCounter = 0;
+    private int clientId;
+    private String nom;
+    private String telephone;
+    private String email;
+
+    // Constructeur sans adresse email
+    public Client(String nom, String telephone) {
+        this.clientId = ++clientCounter;
         this.nom = nom;
-        this.tel = tel;
-        this.email = null;
+        this.telephone = telephone;
     }
-    
-    public Client(int numero, String nom, String tel, String email){
-        this.numero = numero;
-        this.nom = nom;
-        this.tel = tel;
+
+    // Constructeur avec adresse email
+    public Client(String nom, String telephone, String email) {
+        this(nom, telephone);
         this.email = email;
     }
-    
-    @Override
-    public String toString(){
-    return "numero : " + numero + " "
-           + "nom : " + nom + " "
-           + "tel : " + tel + " "
-           + "email : " + email + " ";
+
+    public String toString() {
+        return "Client ID: " + clientId + ", Nom: " + nom + ", Téléphone: " + telephone + (email != null ? ", Email: " + email : "");
+    }
+
+    public boolean placerApres(Client autre) {
+        return this.nom.compareToIgnoreCase(autre.nom) > 0;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getTelephone() {
+        return telephone;
     }
 }
