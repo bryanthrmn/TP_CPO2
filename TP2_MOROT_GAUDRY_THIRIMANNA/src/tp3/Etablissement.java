437f343f651d51f1284be472110d4cebf4c02ce7
Bryan THIRIMANNA <85101130+bryanthrmn@users.noreply.github.com>
2024-11-05 15:36:16 +0100

    Update Etablissement.java

--- TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/Etablissement.java
+++ TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/Etablissement.java
@@ -1,5 +1,47 @@
 package tp3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Etablissement {
-    
+    private String nom;
+    private List<Client> clients;
+    private List<RendezVous> planning; // Liste de rendez-vous pour les 7 jours suivants
+
+    public Etablissement(String nom) {
+        this.nom = nom;
+        this.clients = new ArrayList<>();
+        this.planning = new ArrayList<>();
+    }
+
+    public Client ajouterClient(String nom, String telephone) {
+        Client client = new Client(nom, telephone);
+        clients.add(client);
+        return client;
+    }
+
+    public Client ajouterClient(String nom, String telephone, String email) {
+        Client client = new Client(nom, telephone, email);
+        clients.add(client);
+        return client;
+    }
+
+    public Client rechercherClient(String nom, String telephone) {
+        for (Client client : clients) {
+            if (client.getNom().equalsIgnoreCase(nom) && client.getTelephone().equals(telephone)) {
+                return client;
+            }
+        }
+        return null;
+    }
+
+    public void ajouterRendezVous(RendezVous rdv) {
+        planning.add(rdv);
+    }
+
+    public void afficherPlanning() {
+        for (RendezVous rdv : planning) {
+            System.out.println(rdv);
+        }
+    }
 }
