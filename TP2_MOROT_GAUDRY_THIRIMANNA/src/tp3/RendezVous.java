7d528a56cbd56b3228288b7deae9bfb0293b77e9
Bryan THIRIMANNA <85101130+bryanthrmn@users.noreply.github.com>
2024-11-05 15:35:37 +0100

    Update RendezVous.java

--- TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/RendezVous.java
+++ TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/RendezVous.java
@@ -1,5 +1,21 @@
 package tp3;
 
+import java.time.LocalDateTime;
+
 public class RendezVous {
-    
+    private Client client;
+    private Prestation prestation;
+    private LocalDateTime dateHeure;
+    private double prix;
+
+    public RendezVous(Client client, Prestation prestation, LocalDateTime dateHeure) {
+        this.client = client;
+        this.prestation = prestation;
+        this.dateHeure = dateHeure;
+        this.prix = prestation.calculerPrixTotal();
+    }
+
+    public String toString() {
+        return "Rendez-vous pour " + client + " à " + dateHeure + ", Prestation: " + prestation + ", Prix: " + prix + " €";
+    }
 }
