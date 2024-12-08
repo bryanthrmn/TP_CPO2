a5f0ed69722e1f112aea2ef78da7c9d566abf26b
Bryan THIRIMANNA <85101130+bryanthrmn@users.noreply.github.com>
2024-12-08 17:50:39 +0100

    Update RendezVous.java

--- TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/RendezVous.java
+++ TP2_MOROT_GAUDRY_THIRIMANNA/src/tp3/RendezVous.java
@@ -8,22 +8,29 @@ package tp3;
 import java.time.LocalDateTime;
 
 public class RendezVous {
-    private final Client client;
-    private final Prestation prestation;
-    private final LocalDateTime horaire;
+    // Attributs 
+    private final Client client; // Le client associé au rdv
+    private final Prestation prestation; // La prestation demandée pour ce rendez-vous
+    private final LocalDateTime horaire; // L'heure du rendez-vous
 
+    // Constructeur 
     public RendezVous(Client client, Prestation prestation, LocalDateTime horaire) {
         this.client = client;
         this.prestation = prestation;
         this.horaire = horaire;
     }
 
+    // Informations du rendez-vous sous forme de chaîne
     @Override
     public String toString() {
-        return horaire + " - " + client + " - " + prestation + " - Prix: " + prestation.calculerPrix() + "€";
+        // Retourne une chaîne avec l'horaire, les informations du client, la prestation,
+        // et le prix calculé de la prestation en euros
+        return horaire + " - " + client + " - " + prestation + " - Prix: " + prestation.calculerPrix() + "EUR";
     }
 
+    // Méthode pour obtenir les informations du rendez-vous sous forme de chaîne, prêt à être sauvegardé dans un fichier
     public String versFichier() {
+        // Retourne une chaîne formatée avec l'horaire, le numéro du client et les informations sur la prestation avec son prix
         return horaire + "\n" + client.getNumeroClient() + "\n" + prestation + " : " + prestation.calculerPrix();
     }
 }
